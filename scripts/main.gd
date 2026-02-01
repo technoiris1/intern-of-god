7d41ce7692932945c1d1407b688923246be46f45
Manan Sharma <83415076+technoiris1@users.noreply.github.com>
2026-02-02 07:20:30 +0800

    done with rain

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,1 +1,25 @@
 extends Node2D
+
+@onready var rain = $"CPUParticles2D"
+
+var raining := false
+
+func _input(event):
+
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		
+		# start rain
+		if event.pressed:
+			raining = true
+			rain.emitting = true
+		
+		# stop rain
+		else:
+			raining = false
+			rain.emitting = false
+
+
+func _process(delta):
+	# move rain with mouse while holding click
+	if raining:
+		rain.global_position = get_global_mouse_position()
